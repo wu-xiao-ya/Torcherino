47b6f6cbfdd2a886046bfebd90c0ff782f497eda
wu-xiao-ya <176298918+wu-xiao-ya@users.noreply.github.com>
2026-07-16 23:54:27 +0800

    fix: force Forge mod discovery on CatRoom

--- src/main/java/com/sci/torcherino/Torcherino.java
+++ src/main/java/com/sci/torcherino/Torcherino.java
@@ -42,7 +42,7 @@ import java.util.concurrent.ConcurrentHashMap;
 public class Torcherino {
     public static final String MOD_ID = "torcherino";
     public static final String MOD_NAME = "Torcherino";
-    public static final String VERSION = "8.0.0-alpha.1";
+    public static final String VERSION = "8.0.0-alpha.2";
 
     public static boolean logPlacement;
     public static boolean overPoweredRecipe;
