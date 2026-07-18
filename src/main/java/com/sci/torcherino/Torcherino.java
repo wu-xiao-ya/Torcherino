08cff25ed5aea019847e267be2e3da719d71eb33
wu-xiao-ya <176298918+wu-xiao-ya@users.noreply.github.com>
2026-07-18 13:57:24 +0800

    fix: align alpha.9 version metadata

--- src/main/java/com/sci/torcherino/Torcherino.java
+++ src/main/java/com/sci/torcherino/Torcherino.java
@@ -43,7 +43,7 @@ import java.util.concurrent.ConcurrentHashMap;
 public class Torcherino {
     public static final String MOD_ID = "torcherino";
     public static final String MOD_NAME = "Torcherino";
-    public static final String VERSION = "8.0.0-alpha.8";
+    public static final String VERSION = "8.0.0-alpha.9";
 
     public static boolean logPlacement;
     public static boolean overPoweredRecipe;
